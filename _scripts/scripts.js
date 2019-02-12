1ea1077fd2427e5c0484847c4b07d5e05791078f
Eli <ely.nathan93@gmail.com>
2019-02-12 18:02:55 +0000

    Styling and fix JS

--- _scripts/scripts.js
+++ _scripts/scripts.js
@@ -1,36 +1,39 @@
 
-let submitBtn = document.querySelectorAll("input");
+let inputs = document.querySelectorAll("input");
+let links = document.querySelector(".generate-links")
 
-for(let i = 0; i < submitBtn.length; i++ ) {
-  submitBtn[i].addEventListener("input", function () {
+for(let i = 0; i < inputs.length; i++) {
+  inputs[i].addEventListener("input", function () {
+    links.classList.remove("d-none")
+    links.classList.add("d-block")
     let data = [
       {
         "Name": document.querySelector("#name1").value,
         "Spec": document.querySelector("#variant1").value,
-        "Image": document.querySelector("#deposit1").value,
+        "Image": document.querySelector("#image1").value,
         "Monthly": "£" + document.querySelector("#monthly1").value,
-        "Deposit": "£" + document.querySelector("#image1").value
+        "Deposit": "£" + document.querySelector("#deposit1").value
       },
       {
         "Name": document.querySelector("#name2").value,
         "Spec": document.querySelector("#variant2").value,
-        "Image": document.querySelector("#deposit2").value,
+        "Image": document.querySelector("#image2").value,
         "Monthly": "£" + document.querySelector("#monthly2").value,
-        "Deposit": "£" + document.querySelector("#image2").value
+        "Deposit": "£" + document.querySelector("#deposit2").value
       },
       {
         "Name": document.querySelector("#name3").value,
         "Spec": document.querySelector("#variant3").value,
-        "Image": document.querySelector("#deposit3").value,
+        "Image": document.querySelector("#image3").value,
         "Monthly": "£" + document.querySelector("#monthly3").value,
-        "Deposit": "£" + document.querySelector("#image3").value
+        "Deposit": "£" + document.querySelector("#deposit3").value
       },
       {
         "Name": document.querySelector("#name4").value,
         "Spec": document.querySelector("#variant4").value,
-        "Image": document.querySelector("#deposit4").value,
+        "Image": document.querySelector("#image4").value,
         "Monthly": "£" + document.querySelector("#monthly4").value,
-        "Deposit": "£" + document.querySelector("#image4").value
+        "Deposit": "£" + document.querySelector("#deposit4").value
       },
     ]
     createDownloadButton(data)
@@ -42,5 +45,5 @@ function createDownloadButton(data) {
   let blob = new Blob([json], {type: "application/json"});
   let url  = URL.createObjectURL(blob);
 
-  document.querySelector("#submitBtn").href = url;
+  document.querySelector("#genJSON").href = url;
 }
--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,34 +1,37 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 "use strict";
 
-var submitBtn = document.querySelectorAll("input");
+var inputs = document.querySelectorAll("input");
+var links = document.querySelector(".generate-links");
 
-for (var i = 0; i < submitBtn.length; i++) {
-  submitBtn[i].addEventListener("input", function () {
+for (var i = 0; i < inputs.length; i++) {
+  inputs[i].addEventListener("input", function () {
+    links.classList.remove("d-none");
+    links.classList.add("d-block");
     var data = [{
       "Name": document.querySelector("#name1").value,
       "Spec": document.querySelector("#variant1").value,
-      "Image": document.querySelector("#deposit1").value,
+      "Image": document.querySelector("#image1").value,
       "Monthly": "£" + document.querySelector("#monthly1").value,
-      "Deposit": "£" + document.querySelector("#image1").value
+      "Deposit": "£" + document.querySelector("#deposit1").value
     }, {
       "Name": document.querySelector("#name2").value,
       "Spec": document.querySelector("#variant2").value,
-      "Image": document.querySelector("#deposit2").value,
+      "Image": document.querySelector("#image2").value,
       "Monthly": "£" + document.querySelector("#monthly2").value,
-      "Deposit": "£" + document.querySelector("#image2").value
+      "Deposit": "£" + document.querySelector("#deposit2").value
     }, {
       "Name": document.querySelector("#name3").value,
       "Spec": document.querySelector("#variant3").value,
-      "Image": document.querySelector("#deposit3").value,
+      "Image": document.querySelector("#image3").value,
       "Monthly": "£" + document.querySelector("#monthly3").value,
-      "Deposit": "£" + document.querySelector("#image3").value
+      "Deposit": "£" + document.querySelector("#deposit3").value
     }, {
       "Name": document.querySelector("#name4").value,
       "Spec": document.querySelector("#variant4").value,
-      "Image": document.querySelector("#deposit4").value,
+      "Image": document.querySelector("#image4").value,
       "Monthly": "£" + document.querySelector("#monthly4").value,
-      "Deposit": "£" + document.querySelector("#image4").value
+      "Deposit": "£" + document.querySelector("#deposit4").value
     }];
     createDownloadButton(data);
   });
@@ -40,7 +43,7 @@ function createDownloadButton(data) {
     type: "application/json"
   });
   var url = URL.createObjectURL(blob);
-  document.querySelector("#submitBtn").href = url;
+  document.querySelector("#genJSON").href = url;
 }
 
 },{}]},{},[1])
