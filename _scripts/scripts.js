f62902117378c4abdcb2a0ce82c7cb65bdbc6ef3
Eli <ely.nathan93@gmail.com>
2019-02-13 13:14:41 +0000

    Don’t need £ signs

--- _scripts/scripts.js
+++ _scripts/scripts.js
@@ -11,32 +11,32 @@ for (let i = 0; i < inputs.length; i++) {
         "Name": document.querySelector("#name1").value,
         "Spec": document.querySelector("#variant1").value,
         "Image": document.querySelector("#image1").value,
-        "Monthly": "£" + document.querySelector("#monthly1").value,
-        "Deposit": "£" + document.querySelector("#deposit1").value,
+        "Monthly": document.querySelector("#monthly1").value,
+        "Deposit": document.querySelector("#deposit1").value,
         "Months": document.querySelector("#months1").value
       },
       {
         "Name": document.querySelector("#name2").value,
         "Spec": document.querySelector("#variant2").value,
         "Image": document.querySelector("#image2").value,
-        "Monthly": "£" + document.querySelector("#monthly2").value,
-        "Deposit": "£" + document.querySelector("#deposit2").value,
+        "Monthly": document.querySelector("#monthly2").value,
+        "Deposit": document.querySelector("#deposit2").value,
         "Months": document.querySelector("#months2").value
       },
       {
         "Name": document.querySelector("#name3").value,
         "Spec": document.querySelector("#variant3").value,
         "Image": document.querySelector("#image3").value,
-        "Monthly": "£" + document.querySelector("#monthly3").value,
-        "Deposit": "£" + document.querySelector("#deposit3").value,
+        "Monthly": document.querySelector("#monthly3").value,
+        "Deposit": document.querySelector("#deposit3").value,
         "Months": document.querySelector("#months3").value
       },
       {
         "Name": document.querySelector("#name4").value,
         "Spec": document.querySelector("#variant4").value,
         "Image": document.querySelector("#image4").value,
-        "Monthly": "£" + document.querySelector("#monthly4").value,
-        "Deposit": "£" + document.querySelector("#deposit4").value,
+        "Monthly": document.querySelector("#monthly4").value,
+        "Deposit": document.querySelector("#deposit4").value,
         "Months": document.querySelector("#months4").value
       }
     ]
