4884973b54f334c3b94d43c4c56fe0a8ab0a56fa
Eli <ely.nathan93@gmail.com>
2019-02-13 10:03:32 +0000

    Add CSV support

--- _scripts/scripts.js
+++ _scripts/scripts.js
@@ -1,49 +1,78 @@
-
 let inputs = document.querySelectorAll("input");
 let links = document.querySelector(".generate-links")
+let genCSV = document.getElementById("genCSV")
+let data
 
-for(let i = 0; i < inputs.length; i++) {
-  inputs[i].addEventListener("input", function () {
+for (let i = 0; i < inputs.length; i++) {
+  inputs[i].addEventListener("input", function() {
     links.classList.remove("d-none")
     links.classList.add("d-block")
-    let data = [
-      {
+    data = [{
         "Name": document.querySelector("#name1").value,
         "Spec": document.querySelector("#variant1").value,
         "Image": document.querySelector("#image1").value,
         "Monthly": "£" + document.querySelector("#monthly1").value,
-        "Deposit": "£" + document.querySelector("#deposit1").value
+        "Deposit": "£" + document.querySelector("#deposit1").value,
+        "Months": document.querySelector("#months1").value
       },
       {
         "Name": document.querySelector("#name2").value,
         "Spec": document.querySelector("#variant2").value,
         "Image": document.querySelector("#image2").value,
         "Monthly": "£" + document.querySelector("#monthly2").value,
-        "Deposit": "£" + document.querySelector("#deposit2").value
+        "Deposit": "£" + document.querySelector("#deposit2").value,
+        "Months": document.querySelector("#months2").value
       },
       {
         "Name": document.querySelector("#name3").value,
         "Spec": document.querySelector("#variant3").value,
         "Image": document.querySelector("#image3").value,
         "Monthly": "£" + document.querySelector("#monthly3").value,
-        "Deposit": "£" + document.querySelector("#deposit3").value
+        "Deposit": "£" + document.querySelector("#deposit3").value,
+        "Months": document.querySelector("#months3").value
       },
       {
         "Name": document.querySelector("#name4").value,
         "Spec": document.querySelector("#variant4").value,
         "Image": document.querySelector("#image4").value,
         "Monthly": "£" + document.querySelector("#monthly4").value,
-        "Deposit": "£" + document.querySelector("#deposit4").value
-      },
+        "Deposit": "£" + document.querySelector("#deposit4").value,
+        "Months": document.querySelector("#months4").value
+      }
     ]
     createDownloadButton(data)
   });
 }
 
+genCSV.addEventListener("click", function(e) {
+  e.preventDefault();
+  DownloadJSON2CSV(data)
+})
+
+function DownloadJSON2CSV(objArray) {
+  let array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
+  let str = 'Name, Spec, Image, Deposit, Monthly, Months' + '\r\n';
+
+  for (let i = 0; i < array.length; i++) {
+    let line = '';
+
+    for (let index in array[i]) {
+      line += array[i][index] + ',';
+    }
+
+    line.slice(0, line.Length - 1);
+
+    str += line + '\r\n';
+  }
+  window.open("data:text/csv;charset=utf-8," + escape(str))
+}
+
 function createDownloadButton(data) {
   let json = JSON.stringify(data);
-  let blob = new Blob([json], {type: "application/json"});
-  let url  = URL.createObjectURL(blob);
+  let blob = new Blob([json], {
+    type: "application/json"
+  });
+  let url = URL.createObjectURL(blob);
 
   document.querySelector("#genJSON").href = url;
 }
--- docs/assets/scripts/scripts.js
+++ docs/assets/scripts/scripts.js
@@ -1,40 +1,71 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 "use strict";
 
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
 var inputs = document.querySelectorAll("input");
 var links = document.querySelector(".generate-links");
+var genCSV = document.getElementById("genCSV");
+var data;
 
 for (var i = 0; i < inputs.length; i++) {
   inputs[i].addEventListener("input", function () {
     links.classList.remove("d-none");
     links.classList.add("d-block");
-    var data = [{
+    data = [{
       "Name": document.querySelector("#name1").value,
       "Spec": document.querySelector("#variant1").value,
       "Image": document.querySelector("#image1").value,
       "Monthly": "£" + document.querySelector("#monthly1").value,
-      "Deposit": "£" + document.querySelector("#deposit1").value
+      "Deposit": "£" + document.querySelector("#deposit1").value,
+      "Months": document.querySelector("#months1").value
     }, {
       "Name": document.querySelector("#name2").value,
       "Spec": document.querySelector("#variant2").value,
       "Image": document.querySelector("#image2").value,
       "Monthly": "£" + document.querySelector("#monthly2").value,
-      "Deposit": "£" + document.querySelector("#deposit2").value
+      "Deposit": "£" + document.querySelector("#deposit2").value,
+      "Months": document.querySelector("#months2").value
     }, {
       "Name": document.querySelector("#name3").value,
       "Spec": document.querySelector("#variant3").value,
       "Image": document.querySelector("#image3").value,
       "Monthly": "£" + document.querySelector("#monthly3").value,
-      "Deposit": "£" + document.querySelector("#deposit3").value
+      "Deposit": "£" + document.querySelector("#deposit3").value,
+      "Months": document.querySelector("#months3").value
     }, {
       "Name": document.querySelector("#name4").value,
       "Spec": document.querySelector("#variant4").value,
       "Image": document.querySelector("#image4").value,
       "Monthly": "£" + document.querySelector("#monthly4").value,
-      "Deposit": "£" + document.querySelector("#deposit4").value
+      "Deposit": "£" + document.querySelector("#deposit4").value,
+      "Months": document.querySelector("#months4").value
     }];
     createDownloadButton(data);
   });
+}
+
+genCSV.addEventListener("click", function (e) {
+  e.preventDefault();
+  DownloadJSON2CSV(data);
+});
+
+function DownloadJSON2CSV(objArray) {
+  var array = _typeof(objArray) != 'object' ? JSON.parse(objArray) : objArray;
+  var str = 'Name, Spec, Image, Deposit, Monthly, Months' + '\r\n';
+
+  for (var _i = 0; _i < array.length; _i++) {
+    var line = '';
+
+    for (var index in array[_i]) {
+      line += array[_i][index] + ',';
+    }
+
+    line.slice(0, line.Length - 1);
+    str += line + '\r\n';
+  }
+
+  window.open("data:text/csv;charset=utf-8," + escape(str));
 }
 
 function createDownloadButton(data) {
